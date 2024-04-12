aed3fc03be5abbf69dea2816b6466b313c9c9d6b
pablowinter <pablo.winter@gmail.com>
2024-04-12 11:51:48 -0300

    Agregando dashboard module y component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DahsboardComponent } from './layouts/dahsboard/dahsboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DahsboardComponent
   ],
   imports: [
     BrowserModule,
