2f921632c25f044e14f44a8ce501a95f12f806c0
pablowinter <pablo.winter@gmail.com>
2024-04-26 01:30:20 -0300

    Otras Correcciones de Rutas

--- src/app/layouts/dashboard/dashboard-routing.module.ts
+++ src/app/layouts/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   
   {
+    path:'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
     path:'students',
     loadChildren: () => import('./pages/students/students-table/students-table.module').then(m => m.StudentsModule)
   },
@@ -15,6 +19,11 @@ const routes: Routes = [
     path:'classes',
     loadChildren: () => import('./pages/classes/classes.module').then(m => m.ClassesModule)
   },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  }
 ];
 
 @NgModule({
